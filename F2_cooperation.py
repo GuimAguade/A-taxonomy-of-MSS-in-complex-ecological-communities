62a22ac8e066bebf3a8c6df97e353d67f679378c
GuimAguade <136105715+GuimAguade@users.noreply.github.com>
2023-06-09 19:27:47 +0200

    Add files via upload

---
+++ F2_cooperation.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from timeit import default_timer as timer
+import random
+
+
+# THIS CODE GENERATES COMMUNITIES WITH RANDOM INITIAL CONDITIONS UNDER INCREASING MUTUALISM
+# TO OBSERVE A LOCAL-TO-GLOBAL TRANSITION (FIGURE 2A).
+# G AGUADÉ-GORGORIÓ
+
+####################### PARAMETERS AND SIMULATION VALUES ##############################
+
+reps=500 # Number of initial conditions tested for each system
+SP = 50 # Number of species in the initial species pool
+EPS=10.**-5 #Threshold of immigration
+Max_init =10.0 #Total initial abundance, distributed randomly across species
+PRECISION = 2 #NUMBER OF DECIMAL POINTS CONSIDERED FOR SPECIES ABUNDANCE - AVOID OVER-PRECISE STATES
+
+# DEFINING PARAMETERS: 
+varintra = 1.1 #intra-species heterogeneity
+varinter = 1.1 #interaction strength heterogeneity
+
+# Intraspecies parameters: d, g, Aii
+meand=0.1
+vard=varintra 
+meang=1.0 
+varg=varintra
+meanAii=0.5
+varAii=varintra
+meanBii = 0.1
+varBii= varintra
+
+# Intra-species arrays:
+d=np.random.lognormal(mean=np.log(meand), sigma=np.log(vard), size=SP)
+g=np.random.lognormal(mean=np.log(meang), sigma=np.log(varg), size=SP)
+Aii=np.random.lognormal(mean=np.log(meanAii), sigma=np.log(varAii), size=SP)
+Bii=np.random.lognormal(mean=np.log(meanBii), sigma=np.log(varBii), size=SP)
+
+# Set interaction heterogeneity
+varA = varinter
+varB = varinter  
+
+#No competition:
+B=np.zeros((SP,SP)) #cooperation matrix, all interactions uniform between 0 and coop
+np.fill_diagonal(B,Bii) #we keep the diagonal also random!
+
+# What we explore:
+meanAmin = 0.0000001
+meanAmax=0.01
+meanAfrac=100
+
+
+##################################################################################
+startclock = timer()
+
+meanAlist=[]
+
+Biomass=np.zeros((meanAfrac,reps))
+Survivors=np.zeros((meanAfrac,reps))
+Sppbiomass=np.zeros((meanAfrac,reps,SP))
+
+for i in range(0,meanAfrac): # For each mean(A) value:
+    
+    meanA=meanAmin + (i*((meanAmax - meanAmin)/(float(meanAfrac))) )
+    
+    print("avg A: ", meanA," of ",meanAmax)
+    
+    A=np.random.lognormal(mean=np.log(meanA), sigma=np.log(varA), size=(SP,SP)) #cooperation matrix, all interactions uniform between 0 and coop
+    meanAlist.append(np.mean(A)) #compute avg interaction strength before we introduce the diagonal terms.
+    np.fill_diagonal(A,Aii)
+
+    for j in range(0,reps): # Perform experiment many times
+
+        # One simulation: 
+        def run(
+                 S, #Species number
+                 tmax=500, #maximum time
+                 EPS=EPS,**kwargs
+                     ):
+             def eqs(t,x):
+                 dx = (x*np.dot(A,x/(g + x)) ) - (d*x) - (x*(np.dot(B,x)))
+                 dx[x<=EPS]=np.maximum(0,dx[x<=EPS])  #if a species is below threshold, its abundance cannot keep decreasing
+                 return dx                 
+             
+             #Generate random initial conditions of different width
+             width = np.random.uniform(0,2)
+             x0 = [m*width for m in np.random.random(SP)]
+             sol=solve_ivp(fun=eqs, t_span=(0,tmax),y0=x0)
+             time=sol.t
+             trajectories=sol.y
+             return time, trajectories
+    
+        # End of simulation: write spp trajectories
+        time,trajectories = run(SP)
+            
+        # Count final biomass and surviving spp
+        abundances=sum(trajectories[:,-1])
+        survivors = len([m for m in trajectories[:,-1] if m>0.01])
+        
+        for h in range(SP):
+            Sppbiomass[i,j,h]=trajectories[h,-1]
+        Biomass[i,j]=abundances
+        Survivors[i,j]=survivors
+
+
+endclock = timer()
+print("Program runtime", endclock - startclock)
+
+
+################## COOP PLOTS ###############################
+
+fig,((ax1, ax2 , ax3)) = plt.subplots(1, 3, figsize=(15,4))
+
+
+for i in range(0,reps):
+    ax1.scatter(meanAlist, Biomass[:,i],marker="o", s=20, color="grey", alpha=0.1)
+ax1.set_title('Total biomass')
+ax1.set(xlabel='avg coop', ylabel='Final Biomass')
+
+
+for i in range(0,reps):
+    ax2.scatter(meanAlist, Survivors[:,i],marker="o", s=20, color="grey", alpha=0.1)
+ax2.set_title('Surviving species')
+ax2.set(xlabel='avg coop', ylabel='Surviving species')
+ax2.set_xlim(-0.0005, meanAmax)
+
+for i in range(0,reps):
+    for j in range(SP):
+        ax3.scatter(meanAlist, Sppbiomass[:,i,j],marker="o", s=20, color="grey", alpha=0.1)     
+ax3.set_title('Single-spp abundance')
+ax3.set(xlabel='avg coop', ylabel='species abundance')
+
+
+#plt.show()
+
+fig.tight_layout()
+nom = "F2A.png" 
+plt.savefig(nom, format='png')
+plt.close()
+
+
+
---
+++ SI_F17_heatmap_connectivity.py
@@ -0,0 +1,267 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from timeit import default_timer as timer
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+    
+####################### PARAMETERS AND SIMULATION VALUES ##############################
+
+meanAmax=0.5
+meanBmax=0.15
+meanAfrac=40
+
+reps=50
+SP = 50
+
+temps1 = 500
+temps2 = 10
+
+EPS=10.**-20 #Threshold of immigration
+
+CONNECTIVITY = 0.5
+
+PRECISION = 2 #NUMBER OF DECIMAL POINTS CONSIDERED FOR SPECIES ABUNDANCE - AVOID OVER-PRECISE STATES
+
+SI_threshold = SP * 0.01
+
+# DEFINING PARAMETERS:
+varintra = 1.1
+varinter = 1.00000001
+
+# Intraspecies parameters: d, g, Aii
+meand=0.1
+vard=varintra 
+meang=1.0  
+varg=varintra
+meanAii=0.5
+varAii=varintra
+meanBii = 0.1
+varBii= varintra
+
+#Intra-species arrays:
+d=np.random.lognormal(mean=np.log(meand), sigma=np.log(vard), size=SP)
+g=np.random.lognormal(mean=np.log(meang), sigma=np.log(varg), size=SP)
+Aii=np.random.lognormal(mean=np.log(meanAii), sigma=np.log(varAii), size=SP)
+Bii=np.random.lognormal(mean=np.log(meanBii), sigma=np.log(varBii), size=SP)
+
+# interaction heterogeneity
+varA = varinter
+varB = varinter  
+
+##################################################################################
+
+
+# Interaction range:
+
+meanAlist = []
+
+meanBfrac=meanAfrac
+meanBlist = []
+
+Numstates = np.zeros((meanAfrac, meanBfrac)) # Number of different observed states: len(FreqAB)
+Avg_Surv = np.zeros((meanAfrac, meanBfrac))   # Average surviving species: weighted (FreqAB) avg of Surviving list.
+Frac_cycles = np.zeros((meanAfrac, meanBfrac)) # number of runs that end in non-stable behavior
+
+z=0
+while z < meanBfrac:
+    meanB= z*meanBmax/float(meanBfrac)+0.0001
+    print("row: ", z," of ",meanBfrac)
+    startclock = timer()
+    B=np.random.lognormal(mean=np.log(meanB), sigma=np.log(varB), size=(SP,SP)) 
+    meanBlist.append(np.mean(B))
+    np.fill_diagonal(B,Bii) #we keep the diagonal also random!
+    
+    i=0
+    while i<meanAfrac:
+        meanA= i*meanAmax/float(meanAfrac)+0.0001
+        
+        A=np.zeros((SP,SP)) 
+       
+        for row in range(SP):
+            for column in range(SP):
+                coin = np.random.random()
+                if coin<CONNECTIVITY:
+                    A[row][column] = np.random.lognormal(mean=np.log(meanA), sigma=np.log(varA), size=None)
+        if z==0:
+            meanAlist.append(np.mean(A))
+        np.fill_diagonal(A,Aii)
+    
+        StatAB = []     # For given A and B: A matrix with the different states observed
+        FreqAB = []     # For given A and B: An array, how often each state is reached
+        SurvAB = []     # For given A and B: An array, the number of species surviving in each state
+        num_unst = 0
+        
+        for j in range(0,reps): #Perform experiment many times
+            #print(j," of ",reps)
+            # One simulation: 
+            def run(S, tmax=temps1, EPS=EPS,**kwargs):
+                def eqs(t,x):
+                    dx = (x*np.dot(A,x/(g + x)) ) - (d*x) - (x*(np.dot(B,x)))
+                    dx[x<=EPS]=np.maximum(0,dx[x<=EPS])  #if a species is below threshold, its abundance cannot keep decreasing
+                    return dx
+                #Generate random initial conditions, but summing Xinit:
+                x0 = [v for v in np.random.random(SP)]           
+                sol=solve_ivp(fun=eqs, t_span=(0,tmax),y0=x0)
+                time=sol.t
+                trajectories=sol.y
+                return time, trajectories
+    
+            # Simulation: write spp trajectories
+            time,trajectories = run(SP)
+            
+            finalstate = [m for m in trajectories[:,-1]] 
+            cleanstate = list(np.around(np.array(finalstate),PRECISION)) #APPROXIMATION 1: NO DECIMALS TO AVOID OVER-PRECISE STATE DIFFERENTIATION
+            alivestate = [m for m in finalstate if m>0.5] #APPROXIMATION 2 (SIMILAR): ONLY CONSIDER SPECIES THAT ARE BEYOND 1.0 ABUNDANCE (TO AVOID EFFECTS OF MIGRATION?)
+ 
+            #################### CYCLE CHECK: RUN MORE TIME AND OBSERVE IF STATE IS DIFFERENT #########################
+            
+            def run(S,tmax=temps2,EPS=EPS,**kwargs):
+                def eqs(t,x):
+                    dx = (x*np.dot(A,x/(g + x)) ) - (d*x) - (x*(np.dot(B,x)))
+                    dx[x<=EPS]=np.maximum(0,dx[x<=EPS])  #if a species is below threshold, its abundance cannot keep decreasing
+                    return dx
+                #Solve the system of equations:
+                x0 = finalstate           
+                sol=solve_ivp(fun=eqs, t_span=(0,tmax),y0=x0)
+                time=sol.t
+                trajectories=sol.y
+                return time, trajectories
+    
+            # Simulation: write spp trajectories
+            timeplus,trajectoriesplus = run(SP)
+            finalstateplus = [m for m in trajectoriesplus[:,-1]] 
+            
+            ###########################################################################################################
+            
+            # DO WE SEE A NEW STATE, AN ALREADY-SEEN STATE, OR CYCLING/CHAOTIC BEHAVIOUR?
+            
+            # 1: SEE IF STATE IS STABLE IN TIME
+            
+            diff = 0.0
+            for spp in range(SP):
+                diff += 2 * abs ( finalstate[spp] - finalstateplus[spp] ) / (finalstate[spp]+finalstateplus[spp]+0.01)
+                            
+            if diff > 8*SI_threshold: # if Similarity index is higher than threshold, the state is not stable
+                num_unst+=1
+                SurvAB.append(len(alivestate)) #We do not measure data from non-stable results
+          
+          # 2: IF STABLE, MEASURE THE SIMILARITY INDEX WITH ALL PREVIOUS STATES TO SEE IF NEW.
+            else:
+                state = 0
+                SI_perturb = 0.0
+                while state<len(StatAB): # compare all states with new "cleanstate":
+                    SI = 0.0
+                    for spp in range(SP):
+                        SI += 2* abs ( StatAB[state][spp] - cleanstate[spp] ) / (StatAB[state][spp] + cleanstate[spp] + 0.01) #Add the Euclidean distance
+                    
+                    # IF SI IS SMALLER THAN THRESHOLD, THIS IS THE SAME STATE
+                    if SI<SI_threshold:
+                        FreqAB[state]+=1 #Increase frequency by one
+                        SurvAB.append(len(alivestate)) #count it as a (repeated) diversity
+                        state = len(StatAB)+10 # Stop the search
+                    #ELSE, KEEP ON SEARCHING
+                    else:
+                        state+=1
+                
+                if state==len(StatAB): #If we have reached the end of the loop, the state is new.                     
+                    StatAB.append(cleanstate)
+                    SurvAB.append(len(alivestate))
+                    FreqAB.append(1)            
+                
+                
+
+
+        #End of REPS: we have explored the same parameters many times, avg what we've seen
+        Numstates[z,i] = len(FreqAB) 
+        
+        Frac_cycles[z,i] = num_unst / float(reps)
+        
+        #Heatmap 2: What is the average number of surviving spp?
+        real_surv = []
+        for i1 in range(len(SurvAB)):
+            for i2 in range(len(FreqAB)):
+                real_surv.append(SurvAB[i1]) # We are creating the real list of events: how many times each is observed (like a weighted avg)
+        Avg_Surv[z,i] = np.mean(real_surv)
+
+        i+=1
+    z+=1    
+        
+        
+    endclock = timer()
+    print("Line runtime", endclock - startclock)
+
+################## PLOTS ###############################
+
+#Figure 1: number of states
+
+fig,(ax1,ax2,ax3)= plt.subplots(1,3,figsize=(24,8))
+
+im1 = ax1.imshow(Numstates)
+stra = ["{:.3f}".format(i) for i in meanAlist]
+strb = ["{:.3f}".format(i) for i in meanBlist]
+# Show all ticks and label them with the respective list entries
+ax1.set_xticks(np.arange(len(meanAlist)))
+ax1.set_yticks(np.arange(len(meanBlist)))
+ax1.set_xticklabels(stra)
+ax1.set_yticklabels(strb)
+# Rotate the tick labels and set their alignment.
+plt.setp(ax1.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+ax1.invert_yaxis()
+divider = make_axes_locatable(ax1)
+cax = divider.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(im1, cax=cax, orientation='vertical')
+ax1.set_ylabel('avg competition')
+ax1.set_xlabel('avg cooperation')
+plt.gca().invert_yaxis()
+ax1.set_title("Number of observed states")
+
+
+im2 = ax2.imshow(Frac_cycles, cmap="cividis")
+stra = ["{:.3f}".format(i) for i in meanAlist]
+strb = ["{:.3f}".format(i) for i in meanBlist]
+# Show all ticks and label them with the respective list entries
+ax2.set_xticks(np.arange(len(meanAlist)))
+ax2.set_yticks(np.arange(len(meanBlist)))
+ax2.set_xticklabels(stra)
+ax2.set_yticklabels(strb)
+# Rotate the tick labels and set their alignment.
+plt.setp(ax2.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+ax2.invert_yaxis()
+divider = make_axes_locatable(ax2)
+cax = divider.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(im2, cax=cax, orientation='vertical')
+ax2.set_ylabel('avg competition')
+ax2.set_xlabel('avg cooperation')
+plt.gca().invert_yaxis()
+ax2.set_title("Fraction of cycling simulations")
+
+im3 = ax3.imshow(Avg_Surv)
+#ax1.plot(meanAlist, Bcritplus, label="bistab. threshold", linewidth=2, color="white")
+#ax1.plot(meanAlist, Bcritminus, label="bistab. threshold", linewidth=2, color="white", linestyle="--")
+stra = ["{:.3f}".format(i) for i in meanAlist]
+strb = ["{:.3f}".format(i) for i in meanBlist]
+# Show all ticks and label them with the respective list entries
+ax3.set_xticks(np.arange(len(meanAlist)))
+ax3.set_yticks(np.arange(len(meanBlist)))
+ax3.set_xticklabels(stra)
+ax3.set_yticklabels(strb)
+# Rotate the tick labels and set their alignment.
+plt.setp(ax3.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+ax3.invert_yaxis()
+divider = make_axes_locatable(ax3)
+cax = divider.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(im3, cax=cax, orientation='vertical')
+ax3.set_ylabel('avg competition')
+ax3.set_xlabel('avg cooperation')
+plt.gca().invert_yaxis()
+ax3.set_title("Avg surviving species")
+
+
+fig.tight_layout()
+nom = "SI001_F008_C05.png" 
+plt.savefig(nom, format='png')
+plt.close()
+
